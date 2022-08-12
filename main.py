fb735185d92a3aebceee07b29f08b663d9978842
Hiram Alejandro <71841587+ScarmMBA@users.noreply.github.com>
2022-08-12 17:07:32 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,26 +4,20 @@ import json
 api = requests.get('http://api.openweathermap.org/data/2.5/forecast?lat=25,7272429&lon=-100,311263&appid=e552413b18a80652918bb9f1ddf10010&units=metric&lang=sp&cnt=1')
 json_data = json.loads(api.content)
 
-def tiempoCiudades(ciudad):
-	ciudad = json_data['city']
-	descripcion = json_data['list']
-	estadoCielo = '' 
-	temperatura = ''
-	city = 'San Nicolás de los Garza'
+ciudad = json_data['city']['name']
+descripcion = json_data['list']
+
+for data in descripcion:
+	print( data['weather'])
+	clima = data['weather']
+	for data2 in clima:
+		descript = data2['description']
+
 	
-	for data in descripcion:
-		nombre = ciudad['name']
-		if nombre == city:
-			estadoCielo = descripcion['weather.description']
-			temperatura = data['temp_max']['temp_min']
 
 
-	text = f'Prediccion de hoy en {ciudad} \nCielo {estadoCielo} temperatura {temperatura}'
-	print(f"Prediccion de hoy en { ciudad['name'] } \n Cielo: {estadoCielo} \n Temperatura: {temperatura}")
-	return textoFinal
-
-#tiempo = tiempoCiudades('description')
+texto = f'Hoy en {ciudad} \nEl pronostico es el siguiente: {descript}'
 
 #Version actualizada para enviarse la informacion
 requests.post('https://api.telegram.org/bot5585839781:AAE42khIUAXBDtyMP5E1WGgjGEMCUflQYhc/sendMessage', 
-             data = { 'chat_id' : '-1001677873072', 'text' : tiempo })	
+             data = { 'chat_id' : '-1001677873072', 'text' : texto})	
