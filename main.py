39055e03d804e68fe285e64b134ef033d5b39adf
Hiram Alejandro <71841587+ScarmMBA@users.noreply.github.com>
2022-08-13 00:03:36 -0500

    Update main.py
    Falta sacar la temperatura, falla de String must be int la cual se debe arreglar

--- main.py
+++ main.py
@@ -7,17 +7,38 @@ json_data = json.loads(api.content)
 ciudad = json_data['city']['name']
 descripcion = json_data['list']
 
+
+#probabilidad de lluvia
 for data in descripcion:
-	print( data['weather'])
-	clima = data['weather']
-	for data2 in clima:
-		descript = data2['description']
-
-	
+  propLluvia = data['pop']
+  propLluvia = propLluvia * 100
+  print(propLluvia)
 
 
-texto = f'Hoy en {ciudad} \nEl pronostico es el siguiente: {descript}'
+for data in descripcion:    #sacar descripcion del clima
+    print( data['weather'])
+    clima = data['weather']
+    for data2 in clima:
+        descript = data2['description']
+
+for data in descripcion:    #fecha y hora del pronostico
+  fechaHora = data['dt_txt']
+    
+
+
+texto1 = f'Hoy en {ciudad} \nEl pronostico es el siguiente: {descript} \nTemperatura: °C \n{fechaHora}'
 
 #Version actualizada para enviarse la informacion
 requests.post('https://api.telegram.org/bot5585839781:AAE42khIUAXBDtyMP5E1WGgjGEMCUflQYhc/sendMessage', 
-             data = { 'chat_id' : '-1001677873072', 'text' : texto})	
+             data = { 'chat_id' : '1535633944', 'text' : texto1})
+
+
+if propLluvia >= 60 and propLluvia <=80:
+  texto2= f'Hay una probabilidad de {propLluvia}% que llueva, vaya preparado'
+  requests.post('https://api.telegram.org/bot5585839781:AAE42khIUAXBDtyMP5E1WGgjGEMCUflQYhc/sendMessage', 
+             data = { 'chat_id' : '1535633944', 'text' : texto2})
+if propLluvia >= 80:
+  texto3= f'Hay una probabilidad de {propLluvia}% que llueva, es muy seguro que llueva, preparese y vaya con precaución'
+  requests.post('https://api.telegram.org/bot5585839781:AAE42khIUAXBDtyMP5E1WGgjGEMCUflQYhc/sendMessage', 
+             data = { 'chat_id' : '1535633944', 'text' : texto3})
+#\nTemp. Minima: {tempMin}°C \nTemp. Máx: {tempMax}
