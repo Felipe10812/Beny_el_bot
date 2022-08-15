faa65717858dff21ccaecf9223c89d82eeb08f31
Hiram Alejandro <71841587+ScarmMBA@users.noreply.github.com>
2022-08-15 17:01:51 -0500

    API's 2
    Tiene las dos apis trabajando 🗡️ 👯‍♂️

--- main.py
+++ main.py
@@ -3,11 +3,24 @@
 import requests
 import json
 
-api = requests.get('http://api.openweathermap.org/data/2.5/forecast?lat=25,7272429&lon=-100,311263&appid=e552413b18a80652918bb9f1ddf10010&units=metric&lang=sp&cnt=1')
-json_data = json.loads(api.content)
+apiActual = requests.get('https://api.openweathermap.org/data/2.5/weather?lat=25,7263685&lon=-100,3119711&appid=e552413b18a80652918bb9f1ddf10010&units=metric&lang=sp&cnt=2')
+json_actual = json.loads(apiActual.content)
 
-ciudad = json_data['city']['name']
-descripcion = json_data['list']
+apiPronostico = requests.get('http://api.openweathermap.org/data/2.5/forecast?lat=25,7272429&lon=-100,311263&appid=e552413b18a80652918bb9f1ddf10010&units=metric&lang=sp&cnt=1')
+json_pron = json.loads(apiPronostico.content)
+
+
+tempActual = json_actual['main']['temp']
+feel = json_actual['main']['feels_like']
+viento =json_actual['wind']['speed']
+description = json_actual['weather']
+
+for item in description:
+  desc = item['description']
+  
+
+ciudad = json_pron['city']['name']
+descripcion = json_pron['list']
 
 
 for datamain in descripcion:
@@ -33,22 +46,22 @@ for data in descripcion:    #fecha y hora del pronostico
   fechaHora = data['dt_txt']
     
 
+texto1 = f'Actualmente en {ciudad}: \nTemp. Actual: {tempActual} °C. \nEsta {desc}\nSensación Térmica de: {feel}°C \nViento: {viento} km/h'
+texto2 = f'Pronostico {fechaHora} en \n{ciudad} \n\nEl pronostico es el siguiente: {descript} \nTemperatura: {temp} °C \nTemp. Minima: {tempMin}°C \nTemp. Máx: {tempMax} \nSensación térmica de: {sensacion}°C '
 
-texto1 = f'Hoy en {ciudad} \nEl pronostico es el siguiente: {descript} \nTemperatura actual: {temp} °C \nTemp. Minima: {tempMin}°C \nTemp. Máx: {tempMax} \n Sensación térmica de: {sensacion}°C \n\n{fechaHora}'
-
-#Version actualizada para enviarse la informacion
 requests.post('https://api.telegram.org/bot5585839781:AAE42khIUAXBDtyMP5E1WGgjGEMCUflQYhc/sendMessage', 
              data = { 'chat_id' : '-1001677873072', 'text' : texto1})
 
+#Version actualizada para enviarse la informacion
+requests.post('https://api.telegram.org/bot5585839781:AAE42khIUAXBDtyMP5E1WGgjGEMCUflQYhc/sendMessage', 
+             data = { 'chat_id' : '-1001677873072', 'text' : texto2})
+
 
 if propLluvia >= 60 and propLluvia <=80:
-  texto2= f'Hay una probabilidad de {propLluvia}% que llueva, vaya preparado'
+  texto3= f'Hay una probabilidad de {propLluvia}% que llueva, vaya preparado'
   requests.post('https://api.telegram.org/bot5585839781:AAE42khIUAXBDtyMP5E1WGgjGEMCUflQYhc/sendMessage', 
-             data = { 'chat_id' : '-1001677873072', 'text' : texto2})
-if propLluvia >= 80:
-  texto3= f'Hay una probabilidad de {propLluvia}% que llueva, es muy seguro que llueva, preparese y vaya con precaución'
+             data = { 'chat_id' : '--1001677873072', 'text' : texto3})
+if propLluvia > 80:
+  texto4= f'Hay una probabilidad de {propLluvia}% que llueva, es muy seguro que llueva, preparese y vaya con precaución'
   requests.post('https://api.telegram.org/bot5585839781:AAE42khIUAXBDtyMP5E1WGgjGEMCUflQYhc/sendMessage', 
-             data = { 'chat_id' : '-1001677873072', 'text' : texto3})
-
-  
-  
+             data = { 'chat_id' : '-1001677873072', 'text' : texto4})
